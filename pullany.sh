c4b9694525cf5588b9a804b5106affe6db0b53f6
Rushing <mingli@ishanshan.com>
2018-09-14 12:35:53 +0800

    update:20180914-04

--- pullany.sh
+++ pullany.sh
@@ -2,15 +2,20 @@
 echo -e "\e[1;44m\e[1;36m====================================================================================================\e[0m"
 echo -e "\e[1;44m\e[1;33m您输入的路径为 $*\e[0m"
 echo -e "\e[1;44m\e[1;36m====================================================================================================\e[0m"
+FAILURE_ARY=()
 FAILURE_TIMES=0 # 全局变量，表示更新是否成功，如果失败，表示失败的次数
 WORKSPACE_END="WORKSPACE_END"
 for WORKSPACE in `echo $* $WORKSPACE_END|sed -n 's/ /\n/gp'|sed '/^$/d'`
 do
 	if [ $WORKSPACE = $WORKSPACE_END ]; then
 		if [ $FAILURE_TIMES -ne 0 ]; then
-			echo -e "\e[1;33m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-			echo -e "\e[1;33m>>>>>>>>>>>>>>>>>>>更新错误发生[\e[1;31m`printf "%-6s $FAILURE_TIMES"`\e[1;33m]次<<<<<<<<<<<<<<<<<<<\e[0m"
-			echo -e "\e[1;33m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+			echo -e "\e[1;31m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+			echo -e "\e[1;31m>>>>>>>>>>>>>>>>>>>更新错误发生[\e[1;5;32m  `printf "%-3s" $FAILURE_TIMES`\e[0;1;31m]次<<<<<<<<<<<<<<<<<<<\e[0m"
+			for (( i=1; i<=${#FAILURE_ARY[@]}; i++))
+			do
+		     		echo -e "\e[1;31m`printf "%-3s" $i、`${FAILURE_ARY[i-1]}\e[0m"
+			done
+			echo -e "\e[1;31m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 		else
 			echo -e "\e[1;32m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 			echo -e "\e[1;32m>>>>>>>>>>>>>>>>>>更新完成,开始快乐的工作吧!<<<<<<<<<<<<<<<<<<\e[0m"
@@ -38,17 +43,17 @@ do
 			do
 				echo -e "\e[1;35m切换到分支 [\e[1;32m$BRANCH\e[1;35m] 进行更新...\e[0m"
 				git checkout $BRANCH
-				# echo -e "\e[1;33m切换分支【\e[1;32m$?\e[1;33m】\e[0m"
 				if [ $? -ne 0 ]; then
+					TEMP_CURRENT_BRANCH=`git branch -l|grep "*"|sed 's/* //'` # 获取当前分支
+					FAILURE_ARY[FAILURE_TIMES]="项目[$CURRENT_PROJECT]分支[$TEMP_CURRENT_BRANCH]切换到[$BRANCH]分支失败"
 					let FAILURE_TIMES++
-					TEMP_CURRENT_BRANCH=`git branch -l|grep "*"|sed 's/* //'`
-					echo -e "\e[1;33m切换到分支[\e[1;32m$BRANCH\e[1;33m]失败,当前分支[\e[1;32m$TEMP_CURRENT_BRANCH\e[1;33m]\e[0m"
+					echo -e "\e[1;4;5;33m切换到分支[\e[1;32m$BRANCH\e[1;33m]失败,当前分支[\e[1;32m$TEMP_CURRENT_BRANCH\e[1;33m]\e[0m"
 				else
 					git pull
-					# echo -e "\e[1;33m更新分支【\e[1;32m$?\e[1;33m】\e[0m"
 					if [ $? -ne 0 ]; then
+						FAILURE_ARY[FAILURE_TIMES]="项目[$CURRENT_PROJECT]分支[$BRANCH]更新失败"
 						let FAILURE_TIMES++
-						echo -e "\e[1;33m更新分支[\e[1;32m$BRANCH\e[1;33m]失败\e[0m"
+						echo -e "\e[1;4;5;33m更新分支[\e[1;32m$BRANCH\e[1;33m]失败\e[0m"
 					fi
 				fi
 			done
